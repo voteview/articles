d00291a54f300c431ee9e40dd49aa8a0fb845a73
Aaron Rudkin <junk_signups@rudkin.ca>
2021-09-20 09:05:39 +0100

    Adds dry run to compiler, changes qualifications for recompile, fixes bug in update_delta

--- compiler.R
+++ compiler.R
@@ -19,7 +19,8 @@ if(!rmarkdown::pandoc_available("1.12.3")) {
   }
 }
 
-process_file_name = function(file_name) {
+# 
+process_file_name = function(file_name, force = FALSE, dryrun = FALSE) {
   # String split to get the name of the folder, the name of the file,
   # and the pre-extension name of the file ("base_name")
   folder_chunks = strsplit(file_name, "/")
@@ -35,39 +36,46 @@ process_file_name = function(file_name) {
   html_version = paste0(folder_name, base_name, ".html")
   json_metadata = paste0(folder_name, base_name, ".json")
   extra_outputs = paste0(folder_name, base_name, "_files")
+
+  # If each of the files exists, then a compiled version is considered to
+  # exist.
+  compiled_exists = file.exists(html_version) &&
+    file.exists(json_metadata) && file.exists(extra_outputs)
+  
+  # If we have JSON metadata, then we can check recompile data in the JSON
+  # metadata
   if(file.exists(json_metadata)) {
     needs_recompile = read_expiry_date(json_metadata)
   } else {
     needs_recompile = TRUE
   }
 
-  compiled_exists = file.exists(html_version) &&
-    file.exists(json_metadata) && file.exists(extra_outputs)
-  
+  # Dummied: Get file's most recent modify date.  
   recent_modify = file.mtime(file_name) > file.mtime(html_version)
-  
-  # Conditions where we need an update:
+
+  # Updates:
   # - Compiled files do not exist
-  # - Compiled files do exist and the Rmd file has changed since the
-  #   HTML version has changed
   # - Compiled files do exist, but the JSON tells us it's time to recompile
-  need_update = ifelse(
-    compiled_exists,
-    ifelse(recent_modify, TRUE, needs_recompile),
-    TRUE)
+  # - Force recompile
+  need_update = !compiled_exists || needs_recompile || force
 
   # Do the update if necessary
   if(need_update) {
-    cat("\tRendering Rmd to HTML... \n")
-    render_file(file_name, folder_name)
-    cat("\tWriting JSON metadata... \n")
-    parse_front_matter(file_name, folder_name, base_name)
-    clean_superfluous_libraries(folder_name, base_name)
+    if(dryrun) {
+      cat("\tWould have compiled, but dryrun is set... \n")
+    } else {
+      cat("\tRendering Rmd to HTML... \n")
+      render_file(file_name, folder_name)
+      cat("\tWriting JSON metadata... \n")
+      parse_front_matter(file_name, folder_name, base_name)
+      clean_superfluous_libraries(folder_name, base_name)
+    }
   } else {
     cat("\tFile not modified, keeping cached version.\n")
     cat(paste0("\tFiles exist? ", compiled_exists, "\n"))
     cat(paste0("\tRmd modified? ", recent_modify, "\n"))
     cat(paste0("\tNeeds recompile (timer)? ", needs_recompile, "\n"))
+    cat(paste0("\tForce recompile? ", force, "\n"))
   }
 
 }
@@ -99,36 +107,47 @@ parse_front_matter = function(file_name, folder_name, base_name) {
   # Write a JSON
   yaml_metadata_list = rmarkdown::yaml_front_matter(file_name)
 
-  #
+  # Tags: this will affect where something shows up
   if(is.null(yaml_metadata_list$tag)) {
     cat("\tWARNING: No tags included in article metadata.\n")
   }
+  
+  # Article title
   if(is.null(yaml_metadata_list$title)) {
     cat("\tWARNING: No title included in article metadata,",
         "defaulting to \"", base_name, "\"\n")
     yaml_metadata_list$title = base_name
   }
+  
+  # Who wrote it
   if(is.null(yaml_metadata_list$author)) {
     cat("\tWARNING: No author included in article metadata,",
         "defaulting to \"Voteview Team\"\n")
     yaml_metadata_list$author = "Voteview Team"
   }
+  
+  # What do we write the date as?
   if(is.null(yaml_metadata_list$original_date)) {
     cat("\tWARNING: No original date in article metadata,",
         "defaulting to", format(Sys.Date(), "%Y-%m-%d"), "\n")
     yaml_metadata_list$original_date = format(Sys.Date(), "%Y-%m-%d")
   }
-  if(is.null(yaml_metadata_list$cadence_update)) {
+  
+  # How often does the article need to be updated?
+  if(is.null(yaml_metadata_list$update_delta)) {
     yaml_metadata_list$update_delta = 7
   }
   update_date = format(Sys.Date() + yaml_metadata_list$update_delta, "%Y-%m-%d")
 
+  # Output data
   json_output_list = list(
     title = yaml_metadata_list$title,
     author = yaml_metadata_list$author,
     description = yaml_metadata_list$description,
     original_date = yaml_metadata_list$original_date,
     date_modified = as.numeric(Sys.time()),
+    # Note one stupid hack: the JSON field is recompile_date, but the yaml
+    # field is update_delta, which here becomes update_date when parsed.
     recompile_date = update_date,
     tags = yaml_metadata_list$tags
   )
@@ -148,7 +167,10 @@ core_loop = function() {
   # Process these
   rmd_process_list = list.files(".", ".Rmd$", recursive = TRUE)
 
-  # Loop through the files we wish to process
+  # Loop through the files we wish to process -- why is this not an imap?
+  # Because we don't include tidyverse as a dependency by default in the
+  # compiler. We could include it since many articles do, but we don't
+  # by default.
   i = 1
   for(file_name in rmd_process_list) {
     cat(paste0("Processing file ", i, "/",
@@ -165,19 +187,33 @@ core_loop = function() {
   cat("Job complete.\n")
 }
 
-# Parse command line options (if called from Rscript)
-parser <- argparse::ArgumentParser()
-parser$add_argument("-f", "--filename", default="", type="character",
-                    help="Name of article Rmarkdown file to process (default processes all articles).")
-args <- parser$parse_args()
-if (args$filename != "") {
-  if (file.exists(args$filename)) {
-    cat(sprintf("Processing %s:\n", args$filename))
-    process_file_name(args$filename)
+parse_arguments_dispatch = function() {
+  # Parse command line options (if called from Rscript)
+  parser <- argparse::ArgumentParser()
+  
+  # Force a single file to update
+  parser$add_argument(
+    "-f", "--filename", default="", type="character",
+    help="Name of article Rmarkdown file to process (default processes all articles).")
+  parser$add_argument(
+    "-d", "--dryrun", action="store_true",
+    help="Don't recompile any articles, just say what you would have done."
+  )
+  args <- parser$parse_args()
+  
+  if (args$filename != "") {
+    if (file.exists(args$filename)) {
+      # Process this file and force it to be rewritten.
+      cat(sprintf("Processing %s:\n", args$filename))
+      process_file_name(args$filename, force=TRUE, dryrun=args$dryrun)
+    } else {
+      stop(sprintf("Rmarkdown file '%s' not found.\n", args$filename))
+    }
   } else {
-    stop(sprintf("Rmarkdown file '%s' not found.\n", args$filename))
-  }
-} else {
-  cat("Processing all articles:\n")
-  core_loop()
+    cat("Processing all articles:\n")
+    core_loop(dryrun=args$dryrun)
+  }  
 }
+
+# Parse arguments or process all files.
+parse_arguments_dispatch()
